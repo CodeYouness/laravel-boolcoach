3446deb1ecde18121a71d92f9c54b81b8413cf35
fedtoscano <federicotoscano611@gmail.com>
2024-09-04 15:40:58 +0200

    aggiunte le fillable a Coach

--- app/Models/Coach.php
+++ app/Models/Coach.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Coach extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'real_name',
+        'nickname',
+        'email',
+        'language',
+        'password',
+        'summary',
+        'price',
+        'is_available'
+    ];
 }
