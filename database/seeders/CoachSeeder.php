281008602816b1e701807f4e70b64e1a547e8923
fedtoscano <federicotoscano611@gmail.com>
2024-09-04 16:34:26 +0200

    modificato coach seeder

--- database/seeders/CoachSeeder.php
+++ database/seeders/CoachSeeder.php
@@ -15,22 +15,37 @@ class CoachSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        $prices = [];
+        $languages = ['italian', 'english', 'spanish', 'german', 'russian', 'corean', 'chinese', 'japanese'];
         for ($i=0; $i < 10; $i++) {
             $data = [
-                'name' => $faker->name(),
+                'name' => $faker->firstName(),
                 'surname' => $faker->lastName(),
                 'nickname' => $faker->unique()->userName(),
                 'email' => $faker->unique()->email(),
-                'language' => $faker->languageCode(),
+                'language' => $faker->randomElement($languages),
                 "password"=> Hash::make($faker->password()),
                 'summary'=> $faker->realTextBetween(1000, 4500),
                 'img_url'=> $faker->imageUrl(),
                 'price'=>$faker->randomFloat(2, 9.99, 24.99),
                 'is_available'=>$faker->boolean(100)
             ];
-
             Coach::create($data);
         }
+
+        //! DATI CUSTOM PER TESTARE l'APPLICAZIONE
+        $customData = [
+            'name' => 'federico',
+            'surname'=>  'toscano',
+            'nickname' => 'toscaf',
+            'email' => 'federico@gmail.com',
+            'language' => 'italian',
+            "password"=> '12345678',
+            'summary'=> 'blablablablablabla',
+            'img_url'=> 'https://cdn.shopify.com/s/files/1/0533/2089/files/img-url-filter.jpg?v=1515074624',
+            'price'=> 9.99,
+            'is_available'=> true
+        ];
+
+        Coach::create($customData);
     }
 }
