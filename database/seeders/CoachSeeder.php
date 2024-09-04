08eee6afe9f280de503244dd0453afd86093a522
fedtoscano <federicotoscano611@gmail.com>
2024-09-04 16:22:30 +0200

    creato CoachSeeder

--- database/seeders/CoachSeeder.php
+++ database/seeders/CoachSeeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Coach;
+use Illuminate\Support\Facades\Hash;
+use Faker\Generator as Faker;
 
 class CoachSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $prices = [];
+        for ($i=0; $i < 10; $i++) {
+            $data = [
+                'name' => $faker->name(),
+                'surname' => $faker->lastName(),
+                'nickname' => $faker->unique()->userName(),
+                'email' => $faker->unique()->email(),
+                'language' => $faker->languageCode(),
+                "password"=> Hash::make($faker->password()),
+                'summary'=> $faker->realTextBetween(1000, 4500),
+                'img_url'=> $faker->imageUrl(),
+                'price'=>$faker->randomFloat(2, 9.99, 24.99),
+                'is_available'=>$faker->boolean(100)
+            ];
+
+            Coach::create($data);
+        }
     }
 }
