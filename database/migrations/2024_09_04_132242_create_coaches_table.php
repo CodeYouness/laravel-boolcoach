5e5763f3214768aba48265ef828235bb16a44794
fedtoscano <federicotoscano611@gmail.com>
2024-09-04 16:22:18 +0200

    modificata migration con aggiunta dei campi name, surname, img_url

--- database/migrations/2024_09_04_132242_create_coaches_table.php
+++ database/migrations/2024_09_04_132242_create_coaches_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('coaches', function (Blueprint $table) {
             $table->id();
-            $table->string('real_name', 50);
+            $table->string('name', 50);
+            $table->string('surname', 50);
             $table->string('nickname', 50)->unique();
-            $table->string('email', 50);
+            $table->string('email', 50)->unique();
             $table->string('language', 50);
             $table->char('password', 255);
             $table->text('summary');
-            $table->unsignedTinyInteger('price');
+            $table->string('img_url');
+            $table->decimal('price', 8, 2, true);
             $table->boolean('is_available');
             $table->timestamps();
         });
