fcf0be3bb47ff32fa7e153a52cef7a9a2cefdac2
AdelMysterio <storliniadelmo@gmail.com>
2024-09-05 10:49:56 +0200

    creato controller di user relative rotte e metodo index

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm mb-5">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('users.index') }}">All Users</a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
