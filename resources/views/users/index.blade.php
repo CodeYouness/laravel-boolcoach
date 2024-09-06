a5ee66f4d16cd151f9ba5cd6be1eba689e172a90
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-06 18:19:55 +0200

    added dashboard general layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,6 +3,39 @@
     BoolCoach
 @endsection
 @section('main-content')
+    <div id="dashboard-wrapper" class="container py-4">
+        <div class="row mb-3">
+            <div class="col-12">
+                <div class="row justify-content-around">
+                    <section class="col-12 col-md-7 card p-3">
+                        <h1 class="text-white">GAMES</h1>
+                    </section>
+                    <section class="col-12 col-md-4 card p-3">
+                        <h1 class="text-white">INFO</h1>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="row justify-content-around">
+                    <div class="col-12 col-md-7">
+                        <div class="row">
+                            <section class="col-12 card p-3 mb-3">
+                                <h1>STATISTICS</h1>
+                            </section>
+                            <section class="col-12 card p-3">
+                                <h1>SPONSORSHIPS</h1>
+                            </section>
+                        </div>
+                    </div>
+                    <section class="col-12 col-md-4 card p-3">
+                        <h1>REVIEWS</h1>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
     {{-- <table class="table table-dark table-striped table-hover text-center">
         <thead>
             <tr>
