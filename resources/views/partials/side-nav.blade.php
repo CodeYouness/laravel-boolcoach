89fe893ab5e35ef2f4a29006ac253cb6d2a330ce
fedtoscano <federicotoscano611@gmail.com>
2024-09-09 13:12:30 +0200

    aggiunto hover + animazioni ai link della navbar

--- resources/views/partials/side-nav.blade.php
+++ resources/views/partials/side-nav.blade.php
@@ -1,74 +1,121 @@
 
 <nav id="side-nav" class="navbar navbar-expand-lg d-flex flex-column">
     <a class="my_logo text-decoration-none text-white fw-bold p-0 mb-4" href="#">Boolcoach</a>
+
+    {{-- ! NAVBAR CONTAINER --}}
     <div class="container-fluid d-flex flex-column flex-grow-1 justify-content-between">
+
+        {{-- ! TOP NAV --}}
         <ul class="navbar-nav d-flex flex-column w-100">
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center">
-                    <a href="{{route('users.index')}}" class="text-white">
-                        <i class="fa-solid fa-house fs-4"></i>
-                    </a>
+
+            {{-- ! DASHBOARD --}}
+            <li class="w-100 nav-item d-flex align-items-center mb-2">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon text-center d-flex align-items-center">
+                        <a href="{{route('users.index')}}">
+                            <i class="fa-solid fa-house"></i>
+                        </a>
+                        <a href="{{route('users.index')}}" class="text-decoration-none flex-grow-1 ms-2" >Dashboard</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="{{route('users.index')}}" class="navbar-custom-hover text-decoration-none fs-4 flex-grow-1 ms-2" >Dashboard</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center">
-                    <a href="" class="text-white">
-                        <i class="fa-solid fa-user text-white fs-4"></i>
-                    </a>
+
+            {{-- ! PROFILE --}}
+            <li class="w-100 nav-item d-flex align-items-center mb-2">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon text-center d-flex align-items-center">
+                        <a href="">
+                            <i class="fa-solid fa-user"></i>
+                        </a>
+                        <a href="{{ route('users.show', auth()->id())}}" class="text-decoration-none  flex-grow-1 ms-2">Profile</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="{{ route('users.show', auth()->id())}}" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Profile</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center text-center">
-                    <a href="" class="text-white">
-                        <i class="fa-solid fa-chart-line text-white fs-4"></i>
-                    </a>
+
+            {{-- ! STATISTICS --}}
+            <li class="w-100 nav-item d-flex align-items-center mb-2">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon text-center d-flex align-items-center">
+                        <a href="">
+                            <i class="fa-solid fa-chart-line"></i>
+                        </a>
+                        <a href="#" class="text-decoration-none flex-grow-1 ms-2">Statistics</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="#" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Statistics</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center">
-                    <a href="{{route('reviews.index')}}" class="text-white">
-                        <i class="fa-solid fa-star text-white fs-4"></i>
-                    </a>
+
+            {{-- ! REVIEWS --}}
+            <li class="w-100 nav-item d-flex align-items-center mb-2">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon text-center d-flex align-items-center">
+                        <a href="{{route('reviews.index')}}">
+                            <i class="fa-solid fa-star"></i>
+                        </a>
+                        <a href="{{route('reviews.index')}}" class="text-decoration-none flex-grow-1 ms-2">Reviews</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="{{route('reviews.index')}}" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Reviews</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center">
-                    <a href="{{route('sponsorships.index')}}" class="text-white">
-                        <i class="fa-solid fa-money-bills text-white fs-4"></i>
-                    </a>
+
+            {{-- ! SPONSORSHIPS --}}
+            <li class="w-100 nav-item d-flex align-items-center mb-2">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon text-center d-flex align-items-center">
+                        <a href="{{route('sponsorships.index')}}">
+                            <i class="fa-solid fa-money-bills"></i>
+                        </a>
+                        <a href="{{route('sponsorships.index')}}" class="text-decoration-none flex-grow-1 ms-2">Sponsorships</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="{{route('sponsorships.index')}}" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Sponsorships</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around">
-                <div class="menu-icon text-center">
-                    <a href="{{route('messages.index')}}" class="text-white">
-                        <i class="fa-solid fa-message text-white fs-4"></i>
-                    </a>
+
+            {{-- ! MESSAGES --}}
+            <li class="nav-item d-flex align-items-center">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon d-flex text-center align-items-center">
+                        <a href="{{route('messages.index')}}">
+                            <i class="fa-solid fa-message"></i>
+                        </a>
+                        <a href="{{route('messages.index')}}" class="text-decoration-none  flex-grow-1 ms-2">Messages</a>
+                    </div>
+                        <div class="bold-line"></div>
                 </div>
-                <a href="{{route('messages.index')}}" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Messages</a>
             </li>
         </ul>
+
+        {{-- ! BOTTOM NAV --}}
         <ul class="navbar-nav d-flex flex-column w-100 mb-3">
-            <li class="nav-item d-flex align-items-center justify-content-around mb-2">
-                <div class="menu-icon text-center">
-                    <a href="" class="text-white">
-                        <i class="fa-solid fa-arrow-right-from-bracket text-white fs-4"></i>
-                    </a>
+
+            {{--! LOGOUT --}}
+            <li class="nav-item d-flex align-items-center">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon d-flex text-center align-items-center">
+                        <a href="">
+                            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        </a>
+                        <a href="" class="text-decoration-none flex-grow-1 ms-2">Logout</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Logout</a>
             </li>
-            <li class="nav-item d-flex align-items-center justify-content-around">
-                <div class="menu-icon text-center text-center">
-                    <a href="" class="text-white">
-                        <i class="fa-solid fa-gear text-white fs-4"></i>
-                    </a>
+
+            {{-- ! SETTINGS --}}
+            <li class="nav-item d-flex align-items-center">
+                <div class="link-container d-flex flex-column">
+                    <div class="w-100 menu-icon d-flex text-center align-items-center">
+                        <a href="" class="text-white">
+                            <i class="fa-solid fa-gear"></i>
+                        </a>
+                        <a href="#" class="text-decoration-none fs-4  flex-grow-1 ms-2">Settings</a>
+                    </div>
+                    <div class="bold-line"></div>
                 </div>
-                <a href="#" class="navbar-custom-hover text-decoration-none fs-4  flex-grow-1 ms-2">Settings</a>
             </li>
         </ul>
+
     </div>
 </nav>
