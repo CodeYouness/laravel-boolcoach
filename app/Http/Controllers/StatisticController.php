d100ff23123baa3203cf4f183d877ae1ab4a4334
CodeYouness <ylijassi@gmail.com>
2024-09-10 08:51:58 +0200

    added controller to get votes by value

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class StatisticController extends Controller
 {
-    public function index(User $user)
-    {
+    public function index(){
         $user = Auth::user();
+        $coachId = $user->id;
 
-        return view('statistics.index', compact('user'));
+        $votesDistribution = DB::table('user_vote')
+            ->join('votes', 'user_vote.vote_id', '=', 'votes.id')
+            ->join('users', 'user_vote.user_id', '=', 'users.id')
+            ->where('users.coach_id', $coachId)
+            ->select('votes.value', DB::raw('COUNT(votes.value) as vote_count'))
+            ->groupBy('votes.value')
+            ->orderBy('votes.value', 'asc')
+            ->get();
+
+        $labels = $votesDistribution->pluck('value')->toArray();
+        $data = $votesDistribution->pluck('vote_count')->toArray();
+
+        return view('statistics.index', compact('labels', 'data'));
     }
 }
