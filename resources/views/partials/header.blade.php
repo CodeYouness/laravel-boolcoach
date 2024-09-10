571086569b3a2b6c89ce113279acd3a401de1d74
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-10 09:39:40 +0200

    changed name in nickname in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header id="app-header" class="d-flex align-items-end pb-lg-2 py-1 py-lg-0">
     <div class="d-flex justify-content-between align-items-center w-100 px-4">
 
-        <h3 class="mb-0 d-lg-none"> Welcome back, {{ Auth::user()->name}}</h3>
-        <h2 class="mb-0 d-none d-lg-block"> Welcome back, {{ Auth::user()->name}}</h2>
+        <h3 class="mb-0 d-lg-none"> Welcome back, {{ Auth::user()->nickname}}</h3>
+        <h2 class="mb-0 d-none d-lg-block"> Welcome back, {{ Auth::user()->nickname}}</h2>
         <div class="d-flex justify-content-end align-items-center">
             <ul class="list-unstyled d-flex mb-0">
                 <li class="mx-2">
