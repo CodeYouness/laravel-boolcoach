03873eeb00e6817d507419598534f0a096453575
fedtoscano <federicotoscano611@gmail.com>
2024-09-11 17:14:18 +0200

    creata API per la show di Game () (mostra il singolo gioco in base all'id e la lista degli users associati)

--- app/Http/Controllers/Api/ApiGameController.php
+++ app/Http/Controllers/Api/ApiGameController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApiGameController extends Controller
 {
     public function index(){
-        $games = Game::all();
+        $query = Game::with('users')
+        ->join('game_user', 'games.id', '=', 'game_user.game_id')
+        ->join('users', 'game_user.user_id', 'users.id')
+        ->select('games.*')
+        ->groupBy('games.name')
+        ->orderBy('games.id')
+        ->get();
 
         return response()->json([
             'message' => 'success',
-            'results' => $games
+            'results' => $query
         ]);
     }
 
     public function show(String $id){
-        $game = Game::findOrFail($id);
+        $game = Game::where('id', '=', $id)
+            ->with('users')
+            ->get();
 
         return response()->json([
             'message' => 'success',
