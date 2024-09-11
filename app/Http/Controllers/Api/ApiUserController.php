238c408d4581600c44931e922ac995bef7abed96
fedtoscano <federicotoscano611@gmail.com>
2024-09-11 10:13:10 +0200

    test sulle query string per le funzioni frontend

--- app/Http/Controllers/Api/ApiUserController.php
+++ app/Http/Controllers/Api/ApiUserController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use SebastianBergmann\CodeCoverage\Node\Builder;
 use App\Models\User;
 use App\Models\Game;
 use App\Models\Review;
 
 class ApiUserController extends Controller
 {
-    public function index(){
-        $users = User::with(['games', 'votes', 'reviews'])->get();
+    public function index(String $id){
+        $users = User::with(['games', 'votes', 'reviews']);
 
         return response()->json([
+            'message'=>'success',
             'message' => 'success',
-            'results' => $users
+            'results' => $users,
+            'apiKey' => 'your-api-key-value'
         ]);
     }
 
@@ -25,6 +29,27 @@ class ApiUserController extends Controller
         return response()->json([
             'message' => 'success',
             'results' => $user
+        ]);
+    }
+
+    public function search(Request $request){
+        $nicknameString = $request->input('nickname');
+        $voteString = $request->input('vote_avg');
+
+        $users = User::with(['games', 'votes', 'reviews'])
+        ->join('user_vote', 'users.id', '=', 'user_vote.user_id')
+        ->join('votes', 'user_vote.vote_id', '=', 'votes.id')
+        ->select('users.*', DB::raw('avg(votes.value) as vote_average' ))
+        ->groupBy('users.id')
+        ->where('users.nickname', 'like', $nicknameString.'%')
+        ->orderBy('vote_average', 'DESC')
+        ->having('vote_average', '>=',  $voteString)
+        ->get();
+
+        return response()->json([
+            'message' => 'success',
+            'results' => $users,
+            'apiKey' => 'your-api-key-value'
         ]);
     }
 }
