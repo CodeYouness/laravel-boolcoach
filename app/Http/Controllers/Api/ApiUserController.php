afc039667bd808f98f7a4e2bdd5146822a5022f9
AdelMysterio <storliniadelmo@gmail.com>
2024-09-13 11:58:49 +0200

    fix visualizzazione immagine show coach

--- app/Http/Controllers/Api/ApiUserController.php
+++ app/Http/Controllers/Api/ApiUserController.php
@@ -45,6 +45,12 @@ class ApiUserController extends Controller
     public function show(String $id){
         $user = User::with(['games', 'votes', 'reviews'])->findOrFail($id);
 
+        if (Str::startsWith($user->img_url, 'avatars')) {
+            $user->img_url = Storage::url($user->img_url);
+            // Storage::url($user->img_url)
+
+        }
+        
         return response()->json([
             'message' => 'success',
             'results' => $user
