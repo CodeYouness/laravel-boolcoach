d8f054b6173fd737ef9bb7f525cac3962c308b21
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-18 16:22:19 +0200

    add sponsorship end date to sponsoredUsers query

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -13,9 +13,9 @@
                         <div class="card mb-5">
                             <div class="card-body">
                                 <h4 class="card-title mb-3">Recensione num. {{$index+1}}</h4>
-                                <h5 class="card-title">Lasciata da: <em>{{$review->username}}</em></h5>
-                                <h6 class="card-title mb-2">il {{$review->created_at->toFormattedDayDateString()}}</h6>
-                                <p class="card-text">"{{$review->description}}"</p>
+                                <h6 class="card-title">Lasciata da: <em>{{$review->username}}</em></h6>
+                                <h6 class="card-title mb-2">il {{$review->created_at->format('d/m/y')}}</h6>
+                                <p class="card-text fs-6">"{{$review->description}}"</p>
                             </div>
                         </div>
                     @endforeach
@@ -29,7 +29,7 @@
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title">{{$vote->lable}}</h5>
-                                    <p class="card-text">{{$vote->created_at->toFormattedDayDateString()}}</p>
+                                    <p class="card-text">{{$vote->created_at->format('d/m/y')}}</p>
                                     <div class="rating-stars">
                                         @for ($i = 1; $i <= 5; $i++)
                                             <i class="fas fa-star {{ $i <= $vote->value ? 'text-warning' : '' }}"></i>
